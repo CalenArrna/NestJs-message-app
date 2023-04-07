a63e6733d8e98570ee46bcd55b3937ad39be97d0
calenarrna <calenarrna@gmail.com>
2023-04-07 21:23:19 +0200

    test(message-controller): added some basic tests for current functionality

--- src/messages/messages.controller.spec.ts
+++ src/messages/messages.controller.spec.ts
@@ -15,4 +15,24 @@ describe('MessagesController', () => {
   it('should be defined', () => {
     expect(controller).toBeDefined();
   });
+
+  it("should return list of messages", function() {
+    expect(controller.listMessages()).toBe("List of messages");
+  });
+
+  it("should handle posted message", function() {
+    const message = {content: "Test"};
+
+    const response = controller.createMessage(message);
+
+    expect(response).toBe(`Got your message: ${message}`);
+  });
+
+  it("should handle calls to an ID", function() {
+    const id = '12';
+
+    const response = controller.getMessage(id);
+
+    expect(response).toBe(`Your message id is: ${id}`);
+  });
 });
